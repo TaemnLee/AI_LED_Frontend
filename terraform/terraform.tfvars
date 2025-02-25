609d33ac4f511c30eed496b02db7aca4b05337db
Sunho Kim <107231365+sunhome243@users.noreply.github.com>
2025-02-24 22:33:27 -0500

    terraform init + github actions init.

---
+++ terraform/terraform.tfvars
@@ -0,0 +1,6 @@
+bucket_name = "terraform-static-website-20251"
+
+common_tags = {
+    Name = "terraform-static-website-20251"
+    Environment = "Dev"
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,9 @@
+variable "bucket_name" {
+  description = "The name of the S3 bucket"
+  type        = string
+}
+
+variable "common_tags" {
+  description = "The common tags to apply to all resources"
+  type        = map(string)
+}
